807baa33857b4afc1694389bda99cd5cede0056c
Wylie Fisher <WFisher@ebsco.com>
2015-03-25 19:47:30 -0400

    removing remotely

--- dev/admin/!dbconnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-	$dbc = mysql_connect('localhost','wylie_thibeault','Li8!mes.') or die ("could not connect to database");
-	mysql_select_db('wylie_thibeault', $dbc) or die ("could not select database");
-
-?>
