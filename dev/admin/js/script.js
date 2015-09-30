6eebaa7533b88f8fef65128fccf3866b2d820d59
Wylie <wylie@dukeofcheese.com>
2015-09-29 22:05:55 -0400

    some basic changes... more needed. lots of cleanup with the markup and CSS will need to happen before I begin working on the real meat of the system

--- dev/admin/js/script.js
+++ dev/admin/js/script.js
@@ -1,9 +1,34 @@
 window.onload = init;
 
 function init() {
-    gobig();
+    dataTypes();
 }
 
-function gobig() {
-    console.log('shazam');
+// some arrays we will need as we go along
+var kindsArr = ['works', 'studio', 'news'];
+
+// loop through the kinds and get all the data available
+function dataTypes() {
+	for(var i = 0; i < kindsArr.length; i++) {
+		// lets get the data and store it locally
+		getData(kindsArr[i]);
+	}
+}
+
+function getData(kind) {
+	var request = new XMLHttpRequest();
+	request.open('GET', '../data/' + kind + '.json');
+
+	request.onreadystatechange = function() {
+		var div = document.getElementById('results');
+		if(this.readyState == this.DONE && this.status == 200) {
+			var type = request.getResponseHeader('Content-Type');
+			if(this.responseText != null) {
+				var json = JSON.parse(this.responseText);
+				var keys = Object.keys(json);
+				localStorage.setItem(kind, JSON.stringify(json));
+			}
+		}
+	}
+	request.send();
 }
