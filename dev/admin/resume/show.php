37d14b5ad0daf76e93f34dce514d147de44b3818
Wylie Fisher <wylie@dukeofcheese.com>
2016-02-21 13:56:46 -0500

    getting this just like develop

--- dev/admin/resume/show.php
+++ dev/admin/resume/show.php
@@ -1,33 +1,24 @@
 <?php
 
+	$content = stripslashes($_POST['content']);
 	$http = $_SERVER[HTTP_HOST];
-	$test = $_POST['content'];
-	$content = stripslashes($test);
-	$myFile = '../../resume-raw.php';
 
-	// if( $http == 'dukeofcheese.com' ) {
-	// 	$myFile = 'http://dukeofcheese.com/dev/pt/resume-raw.php';
-	// } else {
-	// 	$myFile = 'http://peterthibeault.com/resume-raw.php';
-	// }
+	if( $http == 'dukeofcheese.com' ) {
+		$myFile = '../../resume-raw.php';
+	} elseif( $http == 'peterthibeault.com' ) {
+		$myFile = '../../resume-raw.php';
+	}
 
-	$fileHandle = fopen($myFile, 'w');
+	$fh = fopen($myFile, 'w');
+	$stringData = $content;
+	fwrite($fh, $stringData);
+	fclose($fh);
 
-	fwrite($fileHandle, $ultimateJson);
-    fclose($fileHandle);
-
-	// $fh = fopen($myFile, 'w');
-	// $fh = fopen($myFile, 'w') or die("can't open file");
-	// fwrite($fh, $content);
-	// fclose($fh);
-
-	// if( $http == 'dukeofcheese.com' ) {
-	// 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://dukeofcheese.com/dev/pt/admin/cv.php">';
-	// } else {
-	// 	echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://peterthibeault.com/admin/cv.php">';
-	// }
-
-	echo 'Your CV has been updated!';
+	if( $http == 'dukeofcheese.com' ) {
+		echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://dukeofcheese.com/dev/pt/admin/cv.php">';
+	} else {
+		echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://peterthibeault.com/admin/cv.php">';
+	}
 
 	exit;
 
