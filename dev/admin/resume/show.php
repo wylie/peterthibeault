f1dd2054870c59090d9baaf0e125b4745ffa02e4
Wylie Fisher <wylie@dukeofcheese.com>
2016-02-21 16:00:26 -0500

    fixing the wrong POST bit. also switching the URLs to point to the new HTML vs PHP page

--- dev/admin/resume/show.php
+++ dev/admin/resume/show.php
@@ -1,6 +1,7 @@
 <?php
 
 	$http = $_SERVER[HTTP_HOST];
+	$newCV = $_POST['cvContent'];
 
 	if( $http == 'dukeofcheese.com' ) {
 		$myFile = '../../resume-raw.php';
@@ -9,14 +10,14 @@
 	}
 
 	$fh = fopen($myFile, 'w');
-	$stringData = stripslashes($_POST['content']);
+	$stringData = stripslashes($newCV);
 	fwrite($fh, $stringData);
 	fclose($fh);
 
 	if( $http == 'dukeofcheese.com' ) {
-		echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://dukeofcheese.com/dev/pt/admin/cv.php">';
+		echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://dukeofcheese.com/dev/pt/admin/cv.html">';
 	} else {
-		echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://peterthibeault.com/admin/cv.php">';
+		echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://peterthibeault.com/admin/cv.html">';
 	}
 
 	exit;
