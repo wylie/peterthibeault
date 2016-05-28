3bf1f2d534b7ad14d823b0dcfa2978e5d5ae913e
Wylie Fisher <wylie@dukeofcheese.com>
2016-05-28 19:01:35 -0400

    requiring handlebars

--- dev/admin/js/script.js
+++ dev/admin/js/script.js
@@ -2,5 +2,6 @@
 //Load common code that includes config, then load the app logic for this page.
 requirejs(['config'], function (config) {
     requirejs(['jquery']);
+    requirejs(['handlebars']);
     requirejs(['init']);
 });
